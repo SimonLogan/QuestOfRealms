4483625493028a622e44f31fbf0b75c4e84d7ad6
SimonLogan <simonalogan@gmail.com>
2017-05-02 22:21:56 +0100

    Evaluate "acquire item" objective.
    Evaluate "acquire item" objective.

--- assets/QuestOfRealms-plugins/default/objectives.js
+++ assets/QuestOfRealms-plugins/default/objectives.js
@@ -38,10 +38,8 @@ module.exports = {
 
           sails.log.info("game.players:" + JSON.stringify(game.players));
           for (var i=0; i<game.players.length; i++) {
-             if (game.players[i].name !== playerName) {
-                sails.log.info("skipping player " + game.players[i].name);
+             if (game.players[i].name !== playerName)
                 continue;
-             }
 
              var location = objective.params[0].value + "_" +
                             objective.params[1].value;
@@ -72,7 +70,53 @@ module.exports = {
        },
        "Acquire item": function(objective, game, playerName, callback) {
           sails.log.info("in Acquire item()");
-          callback(null);
+
+          sails.log.info("game.players:" + JSON.stringify(game.players));
+          for (var i=0; i<game.players.length; i++) {
+             if (game.players[i].name !== playerName)
+                continue;
+
+             var object = objective.params[0].value;
+             var numRequired = objective.params[1].value;
+
+             sails.log.info("Looking for object: " + object +
+                            ", numRequired: " + numRequired);
+
+             if (game.players[i].inventory === undefined) {
+                callback(null);
+                return;
+             }
+
+             var found = 0;
+             for (j=0; j<game.players[i].inventory.length; j++) {
+                var entry = game.players[i].inventory[j];
+                if (entry.type === object)
+                   found++;
+
+                if (found === numRequired)
+                   break;
+             }
+
+             if (found < numRequired) {
+                callback(null);
+                return;
+             }
+
+             // Mark the objective complete.
+             objective.completed = "true";
+             resp = {
+                player: playerName,
+                description: {
+                   action: "objective completed"
+                },
+                data: {
+                   objective: objective
+                }
+             };
+
+             sails.log.info("in Acquire item() callback value");
+             callback(resp);
+          }
        }
   }
 
